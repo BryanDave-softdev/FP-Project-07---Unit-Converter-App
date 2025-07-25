c7b3167bc80dd850fedb6b248b222b2bb1bc1309
Bryan Dave <hbryandave411@gmail.com>
2025-07-25 08:45:11 +0800

    Unit_converter_app.py

---
+++ unit_converter_app.py
@@ -0,0 +1,26 @@
+# FP PROJECT 07: Unit Converter App
+
+print("=== UNIT CONVERTER APP >==")
+print("1. Celsius to Fahrenheit")
+print("2. Kilometers to Miles")
+print("3. Kilograms to Pounds")
+
+choice = input("Choose an option (1/2/3): ")
+
+if choice == "1":
+celsius = float(input("Enter temperature n Celsius: "))
+fahrenheit = (celsius * 9 / 5) + 32
+print(f"\nResult: {celsius}°C = {fahrenheit:.2f}°F")
+
+elif choice == "2":
+kilometers = float(input("Enter distance in Kilometers: "))
+miles = kilometers * 0.621371
+print(f"\nResult: {kilometers} km = {miles:.2f} miles")
+
+elif choice == "3":
+kilograms = float(input("Enter weight in kilograms: "))
+pounds = kilograms * 2.20462
+print(f"\nResult: {kilograms} kg = {pounds:.2f} pounds")
+
+else:
+print("\nInvalid option. Please run the program again.")
