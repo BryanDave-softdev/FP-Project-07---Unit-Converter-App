df4d4dbd54e0fda0d0792b095cce56dc86effb9e
Bryan Dave <hbryandave411@gmail.com>
2025-08-14 11:31:57 +0800

    Update unit_converter_app.py

--- unit_converter_app.py
+++ unit_converter_app.py
@@ -1,6 +1,6 @@
 # FP PROJECT 07: Unit Converter App
 
-print("=== UNIT CONVERTER APP >==")
+print("=== UNIT CONVERTER APP ===")
 print("1. Celsius to Fahrenheit")
 print("2. Kilometers to Miles")
 print("3. Kilograms to Pounds")
@@ -8,19 +8,19 @@ print("3. Kilograms to Pounds")
 choice = input("Choose an option (1/2/3): ")
 
 if choice == "1":
-celsius = float(input("Enter temperature n Celsius: "))
-fahrenheit = (celsius * 9 / 5) + 32
-print(f"\nResult: {celsius}°C = {fahrenheit:.2f}°F")
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9 / 5) + 32
+    print(f"\nResult: {celsius}°C = {fahrenheit:.2f}°F")
 
 elif choice == "2":
-kilometers = float(input("Enter distance in Kilometers: "))
-miles = kilometers * 0.621371
-print(f"\nResult: {kilometers} km = {miles:.2f} miles")
+    kilometers = float(input("Enter distance in kilometers: "))
+    miles = kilometers * 0.621371
+    print(f"\nResult: {kilometers} km = {miles:.2f} miles")
 
 elif choice == "3":
-kilograms = float(input("Enter weight in kilograms: "))
-pounds = kilograms * 2.20462
-print(f"\nResult: {kilograms} kg = {pounds:.2f} pounds")
+    kilograms = float(input("Enter weight in kilograms: "))
+    pounds = kilograms * 2.20462
+    print(f"\nResult: {kilograms} kg = {pounds:.2f} pounds")
 
 else:
-print("\nInvalid option. Please run the program again.")
+    print("\nInvalid option. Please run the program again.")
